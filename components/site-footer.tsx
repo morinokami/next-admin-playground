e044f0b95fadb4bc51d337014b72489ef7c037f1
Shinya Fujino <shf0811@gmail.com>
2023-10-09 17:18:59 +0900

    Make footer accessible

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -9,7 +9,8 @@ export function SiteFooter() {
       <Separator />
       <footer className="py-6">
         <div className="container flex flex-col items-center justify-between">
-          <Link href="">
+          <Link href="https://github.com/morinokami/next-admin-playground">
+            <span className="sr-only">Go to project&apos;s GitHub repo</span>
             <Icons.gitHub className="h-6 w-6" />
           </Link>
         </div>
